473156ea2c3414eb546e0d7a45c3fd64688dbab0
Vladimir Gorbachev <heepsound@yandex.ru>
2023-10-13 19:18:06 +0300

    Выполнено тестовое задание для Практикума, 3 спринт

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var isReset: Bool = false
+
+    private var counter: Int = 0 {
+        willSet(newValue) {
+            if isReset {
+                addHistory("значение сброшено")
+            } else {
+                if newValue < 0 {
+                    addHistory("попытка уменьшить значение счётчика ниже 0")
+                } else if newValue > counter {
+                    addHistory("значение изменено на +\(newValue - counter)")
+                } else {
+                    addHistory("значение изменено на -\(counter - newValue)")
+                }
+            }
+        }
+        didSet {
+            if counter < 0 {
+                counter = 0
+            }
+            counterLabel.text = "Значение счётчика: \(counter)"
+        }
+    }
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counter = 0;
+        historyTextView.text = "История изменений:"
     }
-
-
+    
+    @IBAction func plusButtonTouchUpInside(_ sender: Any) {
+        isReset = false
+        counter += 1
+    }
+    
+    @IBAction func minusButtonTouchUoInside(_ sender: Any) {
+        isReset = false
+        counter -= 1
+    }
+    
+    @IBAction func clearButtonTouchUpInside(_ sender: Any) {
+        isReset = true
+        counter = 0
+    }
+    
+    func addHistory(_ description: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "dd MMMM yyyy hh:mm:ss"
+        
+        historyTextView.text.append("\n[\(dateFormatter.string(from: Date.now))]: \(description)")
+    }
+    
 }
 
